16942522f14c48332683b76cd2b7ca288da4adf8
Disha <disha.baushali@terralogic.com>
2020-12-18 12:43:16 +0530

    assgnmnt1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, HashRouter, Switch, Route } from "react-router-dom"
+import page1 from "./Components/Page1";
+import page2 from "./Components/Page2";
+import menubar from "./Components/Menubar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <HashRouter>
+        <Switch>
+          
+          <Route path="/" exact component={menubar}/>
+          <Route path="/page1" component={page1} />
+          <Route path="/page2" component={page2} />
+    
+          {/* <Route component={Error} /> */}
+        </Switch>
+      </HashRouter>
+    </Router>
+  
+        
+        
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Page2.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import  "./Menubar";
+import "./style.css";
+
+class page2 extends Component {
+    render() {
+        return (
+            <div className="page2">
+            <h1 className="p2">page2</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default page2;
